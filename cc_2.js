58bf82d81e41d2d210472c76fe1722cb188529df
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-09-08 13:50:47 -0500

    Add output functionality to display bill, tip, and total

--- cc_2.js
+++ cc_2.js
@@ -2,10 +2,14 @@
 
 let billAmount = 100;
 let tip
+let totalBillAmount = (tip + billAmount);
 
 // Created an if statement to calculate a 15% tip if the bill amount is within the $50-$300 range.
 if(50 <= billAmount <= 300){
     tip = billAmount * .15
-    console.log("Tip Amount:",tip);
+    console.log("The bill is:$",billAmount); // 
+    console.log("Tip Amount:$",tip);
+    console.log("The total bill amount is:$",totalBillAmount);
 }
-    
+  
+
